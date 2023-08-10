b27c323036ec33072abd7959bec5849c0b7cbc21
Camila <camiart@gmail.com>
2023-08-09 21:30:53 -0300

    exercicio02

---
+++ modulo01Semana10/src/components/header/index.jsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+function Header(){
+    return(
+        <div className="header">
+          <img src="/src/img/cataventos.png" alt=""/>  
+        </div>
+        
+    )
+}
+
+export default Header;
