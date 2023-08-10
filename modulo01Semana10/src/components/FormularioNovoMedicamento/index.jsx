72924600c77562ad7c2491c575d4082883a1559a
Camila <camiart@gmail.com>
2023-08-09 23:07:40 -0300

    exercicio05

--- modulo01Semana10/src/components/FormularioNovoMedicamento/index.jsx
+++ modulo01Semana10/src/components/FormularioNovoMedicamento/index.jsx
@@ -1,4 +1,9 @@
+import {useState} from 'react';
+import { MedicamentosContext } from '../../context/medicamentoContext';
+
 function FormularioNovoMedicamento() {
+
+    const{AdicionarMedicamento} = useContext(MedicamentosContext);
 
     const [nome, setNome] = useState('');
     const [laboratorio, setLaboratorio] = useState('');
@@ -7,7 +12,10 @@ function FormularioNovoMedicamento() {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        
+        AdicionarMedicamento(nome, laboratorio, preco);
+        setNome('');
+        setLaboratorio('');
+        setPreco(0);
         
         }
 
@@ -17,7 +25,7 @@ function FormularioNovoMedicamento() {
         <form onSubmit={handleSubmit}>
             <input type="text" placeholder='Qual o nome do medicamento que você quer cadastrar?'value={nome} onChange={(e)=> setNome(e.target.value)}/>
             <input type="text" placeholder='Qual o nome do Laboratório do medicamento?' value={laboratorio} onChange={(e)=> setLaboratorio(e.target.value)}/>
-            <input type="number" placeholder='Qual o preço do medicamento?' value={preco} onChange={(e)=> setPreco(e.target.value)}/>
+            <input type="number" placeholder='Qual o preço do medicamento?' value={preco} onChange={(e)=> setPreco(Number(e.target.value))}/>
             <button>Cadastrar novo</button>
         </form>
         </>
--- modulo01Semana10/src/context/medicamentoContext.jsx
+++ modulo01Semana10/src/context/medicamentoContext.jsx
@@ -5,9 +5,29 @@ export const MedicamentosContext = createContext()
 export const MedicamentosContextProvider = ({children}) => {
 
     const [listaMedicamentos, setListaMedicamentos] = useState([])
+
+    const AdicionarMedicamento = (nome, laboratorio, preco) =>{
+        if(nome.length === "" || laboratorio.length === "" || preco.length === 0){
+            alert("Preencha todos os campos")
+            return
+        }
+        const novoMedicamento = {
+            id: listaMedicamentos.length + 1,
+            nome: nome,
+            laboratorio: laboratorio,
+            preco: preco,
+            favorito: false
+    }
+
+    const novaLista = [...listaMedicamentos, novoMedicamento]
+    console.log(novaLista)
+    setListaMedicamentos(novaLista)
+    alert("Medicamento adicionado com sucesso")
+
     return (
-        <MedicamentosContext.Provider value={{listaMedicamentos, setListaMedicamentos}}>
+        <MedicamentosContext.Provider value={{listaMedicamentos, AdicionarMedicamento}}>
             {children}
         </MedicamentosContext.Provider >
      )
+}
 }
