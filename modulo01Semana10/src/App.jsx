a71d55a95960214108423a5cebe23d859e407f92
Camila <camiart@gmail.com>
2023-08-09 22:00:15 -0300

    exercicio03

--- modulo01Semana10/src/App.jsx
+++ modulo01Semana10/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/header'
+import FormularioNovoMedicamento from './components/FormularioNovoMedicamento'
 
 function App() {
   
   return (
     <>
       <Header />
+      <FormularioNovoMedicamento />
     </>
   )
 }
---
+++ modulo01Semana10/src/components/FormularioNovoMedicamento/index.jsx
@@ -0,0 +1,26 @@
+function FormularioNovoMedicamento() {
+
+    const [nome, setNome] = useState('');
+    const [laboratorio, setLaboratorio] = useState('');
+    const [preco, setPreco] = useState(0);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        }
+
+    return (
+        <>
+        <h1>Formulário de Cadastro de Medicamentos</h1>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='Qual o nome do medicamento que você quer cadastrar?'value={nome} onChange={(e)=> setNome(e.target.value)}/>
+            <input type="text" placeholder='Qual o nome do Laboratório do medicamento?' value={laboratorio} onChange={(e)=> setLaboratorio(e.target.value)}/>
+            <input type="number" placeholder='Qual o preço do medicamento?' value={preco} onChange={(e)=> setPreco(e.target.value)}/>
+            <button>Cadastrar novo</button>
+        </form>
+        </>
+        
+    )
+}
+
+export default FormularioNovoMedicamento;
